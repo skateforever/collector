1530673972bf0cc3c948aac007eb7a26f3926d44
Leandro Inácio <carvalho.inacio@gmail.com>
2025-09-14 16:59:18 -0300

    Trying to fix webapp check

--- functions/webapp_check.sh
+++ functions/webapp_check.sh
@@ -36,9 +36,11 @@ webapp_alive(){
             fi
             if [ "${webapp_tool_detection}" == "httpx" ]; then
                 echo "echo \"${subdomain}\" | httpx "${httpx_options[@]}" -p $(echo "${webapp_port_detect[@]}" | sed 's/ /,/g') -status-code | \
-                    sed 's/\[// ; s/]//' | grep -Ei \"^(http|https)://${subdomain}.*[0-9]{3}$\" >> "${tmp_dir}/webapp_status_tmp.txt"" >> "${log_execution_file}"
+                    awk '{print $1"\t"$2}' | sed 's/\[// ; s/]//' | grep -Ei \"^(http|https):\/\/${subdomain}.*[0-9]{3}$\" \
+                    >> "${tmp_dir}/webapp_status_tmp.txt"" >> "${log_execution_file}"
                 echo "${subdomain}" | httpx "${httpx_options[@]}" -p $(echo "${webapp_port_detect[@]}" | sed 's/ /,/g') -status-code | \
-                    sed 's/\[// ; s/]//' | grep -Ei "^(http|https)://${subdomain}.*[0-9]{3}$" >> "${tmp_dir}/webapp_status_tmp.txt" 2>> "${log_execution_file}"
+                    awk '{print $1"\t"$2}' | sed 's/\[// ; s/]//' | grep -Ei "^(http|https):\/\/${subdomain}.*[0-9]{3}$" \
+                    >> "${tmp_dir}/webapp_status_tmp.txt" 2>> "${log_execution_file}"
             fi
         done
 
