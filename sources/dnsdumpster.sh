d7d8f46ab42f8a8e952760394a614eff767a1303
Leandro Inácio <carvalho.inacio@gmail.com>
2025-09-20 12:24:10 -0300

    Trying to fix dnsdumpster

--- sources/dnsdumpster.sh
+++ sources/dnsdumpster.sh
@@ -10,16 +10,19 @@
 
 dnsdumpster-src(){
             echo -ne "${yellow}$(date +"%d/%m/%Y %H:%M")${reset} ${red}>>${reset} Executing dns dumpster... "
+            echo "curl ${curl_options[@]} -L ${dnsdumpster_url} | grep -i -P  \"csrfmiddlewaretoken\" | grep -Po \'(?<=value=\")[^\"]*(?=\")\'" >> "${log_execution_file}"
+            dnsdumpster_csrf_token=$(curl "${curl_options[@]}" -L "${dnsdumpster_url}" | grep -i -P  "csrfmiddlewaretoken" | grep -Po '(?<=value=")[^"]*(?=")')
+            echo "${dnsdumpster_csrf_token}" >> "${log_execution_file}"
             echo "curl ${curl_options[@]} -X POST -b \"csrftoken=${dnsdumpster_csrf_token}\" -H 'Accept: */*' -H 'Content-Type: application/x-www-form-urlencoded' \
                 -H \"Origin: ${dnsdumpster_url}\" -H \"Referer: ${dnsdumpster_url}\" \
                 --data-binary \"csrfmiddlewaretoken=${dnsdumpster_csrf_token}&targetip=${domain}&user=free\" ${dnsdumpster_url}" >> "${log_execution_file}"
-            dnsdumpster_csrf_token=$(curl "${curl_options[@]}" -L "${dnsdumpster_url}" | grep -i -P  "csrfmiddlewaretoken" | grep -Po '(?<=value=")[^"]*(?=")')
             curl "${curl_options[@]}" -X POST -b "csrftoken=${dnsdumpster_csrf_token}" -H 'Accept: */*' -H 'Content-Type: application/x-www-form-urlencoded' \
                 -H "Origin: ${dnsdumpster_url}" -H "Referer: ${dnsdumpster_url}" \
                 --data-binary "csrfmiddlewaretoken=${dnsdumpster_csrf_token}&targetip=${domain}&user=free" "${dnsdumpster_url}" \
                 | grep -Po '<td class="col-md-4">\K[^<]*' | sort -u \
                 | grep "${domain}" >> "${tmp_dir}/dnsdumpster_output.txt"
             echo "Done!"
+            sleep 1
 }
 
 dnsdumpster-src
