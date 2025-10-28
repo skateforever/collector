883fdf1815d70785ffe42ddcba0cad7a9b03b34e
Leandro Inácio <carvalho.inacio@gmail.com>
2025-10-28 16:11:09 -0300

    Fixing the code pattern

--- sources/tlsx.sh
+++ sources/tlsx.sh
@@ -4,14 +4,14 @@
 # This file is an essential part of collector's execution!  #
 # And is responsible to get the functions:                  #
 #                                                           #
-#   * tlsx-src                                         #
+#   * tlsx-src                                              #
 #                                                           #
 #############################################################            
 
 tlsx-src(){
     echo -ne "${yellow}$(date +"%d/%m/%Y %H:%M")${reset} ${red}>>${reset} Executing tlsx... "
     echo -e "\n tlsx ${tlsx_options[@]} -d ${domain}" >> "${log_execution_file}"
-    echo "${domain}" | tlsx "${tlsx_options[@]}" > "${tmp_dir}/tlsx_output.json" 2>> "${log_execution_file}"
+    tlsx "${tlsx_options[@]}" -d "${domain}" > "${tmp_dir}/tlsx_output.json" 2>> "${log_execution_file}"
     echo "Done!"
     sleep 1
 }
