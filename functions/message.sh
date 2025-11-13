2f110e95ed94bf64c8e755925541eff43de27f86
Leandro Inácio <carvalho.inacio@gmail.com>
2025-11-13 16:20:18 -0300

    Fix message

--- functions/message.sh
+++ functions/message.sh
@@ -12,6 +12,10 @@
 message(){
     target="$1"
     status="$2"
+    if [ "${status}" == "start" ]; then
+        echo -e "${yellow}$(date +"%d/%m/%Y %H:%M")${reset} ${red}>>${reset} The reconnaissance on ${yellow}${target}${reset} ${green}started!${reset}"
+        echo "The reconnaissance on ${target} started at $(date +"%Y%m%d %H:%M")!" | notify -nc -silent -id "${notify_recon_channel}" > /dev/null
+    fi
     if [ "${status}" == "finished" ]; then
         echo -e "${yellow}$(date +"%d/%m/%Y %H:%M")${reset} ${red}>>${reset} The reconnaissance on ${yellow}${target}${reset} ${green}finished!${reset}"
         echo "The reconnaissance on ${target} finished at $(date +"%Y%m%d %H:%M")!" | notify -nc -silent -id "${notify_recon_channel}" > /dev/null
@@ -19,10 +23,6 @@ message(){
     if [ "${status}" == "failed" ]; then
         echo -e "${yellow}$(date +"%d/%m/%Y %H:%M")${reset} ${red}>>${reset} The reconnaissance on ${yellow}${target}${reset} ${red}failed!${reset}"
         echo "The reconnaissance on ${target} failed at $(date +"%Y%m%d %H:%M")!" | notify -nc -silent -id "${notify_recon_channel}" > /dev/null
-    fi
-    if [ "${status}" == "start" ]; then
-        echo -e "${yellow}$(date +"%d/%m/%Y %H:%M")${reset} ${red}>>${reset} ${green}The reconnaissance on${reset} ${yellow}${target}${reset} ${green}started!${reset}"
-        echo "The reconnaissance on ${target} started at $(date +"%Y%m%d %H:%M")!" | notify -nc -silent -id "${notify_recon_channel}" > /dev/null
     fi
     unset target
     unset status
