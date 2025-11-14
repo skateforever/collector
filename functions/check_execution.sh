0f7f7e02efa24851c92ec971fa187d67db823ba6
Leandro Inácio <carvalho.inacio@gmail.com>
2025-11-14 09:32:00 -0300

    Trying to make the collector execution more straightforward and with more explanatory messages.

--- functions/check_execution.sh
+++ functions/check_execution.sh
@@ -58,27 +58,28 @@ check_parameter_conflicts(){
 check_parameter_dependency(){
     # Basic Execution Check
     if  [[ "${args}" =~ (-d|--domain|-dl|--domain-list) ]] && \
-        [[ ! -s "${report_dir}/domains_alive.txt" && ! "${args}" =~ (-r|--recon) ]] && \
-        [[ ! "${args}" =~ (-wd|--webapp-discovery|-we|--webapp-enum) ]]; then
+        [[ ! -d "${report_dir}" && ( ! "${args}" =~ (-r|--recon) || -z "${recon_check}" || "${recon_check}" == "no" ) ]]; then
         echo -e "You are trying to perform recon, but don't have a structure and are using a different parameter than -r|--recon with domain options."
         echo -e "You need to perform at least a basic run to get the subdomain discovered and continue the rest of the activities.\n"
         usage
     fi
     
     # Web Application Discovery Check
-    if [[ "${args}" =~ (-wd|--webapp-discovery) ]] && \
-        [[ ! -d "${report_dir}" && ! "${args}" =~ (-r|--recon) ]] ; then
-        echo -e "You are trying to perform an action where the basic recognition structure does not yet exist; run the collector again with the -r|--recon option.\n"
+    if [[ "${args}" =~ (-d|--domain|-dl|--domain-list) ]] && \
+        [[ "${webapp_discovery_check}" == "yes" || "${args}" =~ (-wd|--webapp-discovery) ]] && \
+        [[ ! -d "${report_dir}" && ( ! "${args}" =~ (-r|--recon) || -z "${recon_check}" || "${recon_check}" == "no" ) ]]; then
+        echo -e "You are trying to perform web application discovery where the basic recognition structure does not yet exist, run the collector again with the -r|--recon option.\n"
         usage
     fi
 
-    if [[ "${args}" =~ (-wd|--webapp-discovery) ]] && \
+    if [[ "${args}" =~ (-d|--domain|-dl|--domain-list) ]] && \
+        [[ "${webapp_discovery_check}" == "yes" || "${args}" =~ (-wd|--webapp-discovery) ]] && \
         [[ ! -s "${report_dir}/domains_alive.txt" && ! "${args}" =~ (-r|--recon) ]] ; then
         echo -e "You are trying to run web application discovery without having previously run recon, use the -r|--recon option and run again.\n"
         usage
     fi
     
-    if [[ "${args}" =~ (-wd|--webapp-discovery) ]] && \
+    if [[ "${webapp_discovery_check}" == "yes" || "${args}" =~ (-wd|--webapp-discovery) ]] && \
         [[ ! "${args}" =~ (-wld|--webapp-long-detaction|-wsd|--webapp-short-detection) ]]; then
         echo -e "You are trying to find out which web applications are active, but forgot to specify which ports to test."
         echo -e "Choose one of the options (-wld|--webapp-long-detection or -wsd|--webapp-short-detection) and run again.\n"
@@ -87,37 +88,33 @@ check_parameter_dependency(){
 
     if [[ "${args}" =~ (-wld|--webapp-long-detaction|-wsd|--webapp-short-detection)  ]] && \
         [[ ! "${args}" =~ (-wd|--webapp-discovery) ]]; then
-        echo -e "You trying to execute collector with web app discovery without at least one valid option to port detection.\n"
+        echo -e "You trying to execute collector to perform web application discovery without setting -wd|--webapp-discovery option.\n"
         usage
     fi
 
     # Web Application Enumeration Check
-    if [[ "${args}" =~ (-we|--webapp-enum) ]] && \
-        [[ ! -s "${report_dir}" && ! "${args}" =~ (-r|--recon) ]] ; then
-        echo -e "You are trying to perform an action where the basic recognition structure does not yet exist; run the collector again with the -r|--recon option.\n"
+    if [[ "${args}" =~ (-d|--domain|-dl|--domain-list) ]] && \
+        [[ "${webapp_enum_check}" == "yes" || "${args}" =~ (-we|--webapp-enum) ]] && \
+        [[ ! -d "${report_dir}" && ( ! "${args}" =~ (-r|--recon) || -z "${recon_check}" || "${recon_check}" == "no" ) ]]; then
+        echo -e "You are trying to perform web application discovery where the basic recognition structure does not yet exist, run the collector again with the -r|--recon option.\n"
         usage
     fi
 
-    if [[ "${args}" =~ (-we|--webapp-enum) ]] && \
-        [[ ! -s "${report_dir}/domain_alive.txt" && ! "${args}" =~ (-r|--recon) ]] ; then
-        echo -e "You are trying to run web application discovery without having previously run recon, use the -r|--recon option and run again.\n"
+    if [[ "${args}" =~ (-d|--domain|-dl|--domain-list) ]] && \
+        [[ "${webapp_enum_check}" == "yes" || "${args}" =~ (-we|--webapp-enum) ]] && \
+        [[ ! -s "${report_dir}/webapp_urls.txt" && ( ! "${args}" =~ (-wd|--webapp-discovery) || -z "${webapp_discovery_check}" || "${webapp_discovery_check}" == "no" ) ]] ; then
+        echo -e "You are trying to run web application enumeration without having previously web application discovery, use the -wd|--webapp-discovery option and run again.\n"
         usage
     fi
 
-    if [[ "${only_webapp_enum}" == "yes" ]] && \
-        [[ ! "${args}" =~ (-wd|--webapp-discovery) && ! -s "${report_dir}/webapp_urls.txt" ]]; then
-        echo -e "Make sure the ${yellow}webapp_urls.txt${reset} exist and isn't empty, or really, we have a problem with script execution."
-        echo -e "Try running the script with the -wd|--webapp-discovery option and run again.\n"
-        usage
-    fi
-
-    if [[ "${only_webapp_enum}" == "yes" ]] && [[ ${#webapp_wordlists[@]} -eq 0 ]]; then
+    if [[ "${webapp_enum_check}" == "yes" || "${args}" =~ (-we|--webapp-enum) ]] && \
+        [[ ${#webapp_wordlists[@]} -eq 0 || ! "${args}" =~ (-wl|--webapp-wordlists) ]]; then
         echo -e "Please, ${yellow}make sure${reset} you have at least one wordlist to web directory and file discovery!\n"
         usage
     fi
 
-    if [[ -n "${url_2_verify}" ]]; then
-        echo -e "You can't use this (-we|--webapp-enum) option with \"-u|--url\"!\n"
+    if [[ "${webapp_enum_check}" == "yes" || "${args}" =~ (-we|--webapp-enum) ]] && [[ -n "${url_2_verify}" ]]; then
+        echo -e "You can't use this -we|--webapp-enum option with \"-u|--url\"!\n"
         usage
     fi
 
