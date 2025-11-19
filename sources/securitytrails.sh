7ffbb1a992290f496b4b2e23075014e602ac9820
Leandro Inácio <carvalho.inacio@gmail.com>
2025-11-19 09:52:44 -0300

    Fix message

--- sources/securitytrails.sh
+++ sources/securitytrails.sh
@@ -14,14 +14,16 @@ securitytrails-src(){
         echo -e "\ncurl ${curl_options[@]} -H 'Accept: application/json' -H \"APIKEY: ${securitytrails_api_key}\" \
             \"${securitytrails_api_url}/domain/${domain}/subdomains?children_only=false&include_inactive=true\"" \
             >> "${log_execution_file}"
-        securitytrails_api_check=$(curl "${curl_options[@]}" "${securitytrails_api_url}/ping" -H "APIKEY: ${securitytrails_api_key}" -H 'Accept: application/json' | jq -r '.success' 2>> ${log_execution_file})
-        if [ "${securitytrails_api_check}" == "true" ]; then
+        securitytrails_api_check=$(curl "${curl_options[@]}" -H "APIKEY: ${securitytrails_api_key}" -H 'Accept: application/json' "${securitytrails_api_url}/ping" | jq -r '.success' 2>> ${log_execution_file})
+        if [[ -n "${securitytrails_api_check}"]] &&  [[ "${securitytrails_api_check}" == "true" ]] ; then
             curl "${curl_options[@]}" -H 'Accept: application/json' -H "APIKEY: ${securitytrails_api_key}" \
                 "${securitytrails_api_url}/domain/${domain}/subdomains?children_only=false&include_inactive=true" \
                 >> "${tmp_dir}/securitytrails_output.json" 2>> "${log_execution_file}"
             sleep 1
+            echo "Done!"
+        else
+            echo "Fail!"
         fi
-        echo "Done!"
     fi
 }
 
