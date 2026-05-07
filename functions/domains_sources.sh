04f8cb2a94d901dbb66c3e9c5935077f42e4a137
Leandro Inácio <carvalho.inacio@gmail.com>
2026-05-07 08:06:41 -0300

    Remove the vhost file, vhost_check function is in the infra.sh function file

--- functions/domains_sources.sh
+++ functions/domains_sources.sh
@@ -15,7 +15,7 @@ subdomains_recon(){
         echo -e "${yellow}$(date +"%d/%m/%Y %H:%M")${reset} ${red}>>${reset} Initializing the subdomains discovery and this might take a certain time!"
         source_files=(alienvault.sh amass.sh anubis.sh bruteforce.sh builtwith.sh certspotter.sh commoncrawl.sh crt.sh dnsdumpster.sh dnsrepo.sh \
             hackerone.sh hackertarget.sh katana.sh netcraft.sh netlas.sh rapiddns.sh robtex.sh securitytrails.sh shodan.sh subdomaincenter.sh \
-            subfinder.sh tlsx.sh urlfinder.sh urlscan.sh vhost.sh virustotal.sh waybackurls.sh webarchive.sh whoisxmlapi.sh zonetransfer.sh)    
+            subfinder.sh tlsx.sh urlfinder.sh urlscan.sh virustotal.sh waybackurls.sh webarchive.sh whoisxmlapi.sh zonetransfer.sh)
         for src in "${source_files[@]}"; do
             source "${collector_path}/sources/${src}"
         done
