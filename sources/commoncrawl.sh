0fc03850be7c822da15ea4c1334ffeceddec0927
Leandro Inácio <carvalho.inacio@gmail.com>
2026-05-08 13:06:45 -0300

    Fixing message log

--- sources/commoncrawl.sh
+++ sources/commoncrawl.sh
@@ -12,7 +12,7 @@ commoncrawl-src(){
     echo -ne "${yellow}$(date +"%d/%m/%Y %H:%M")${reset} ${red}>>${reset} Executing commoncrawl... "
     unset user_agent
     user_agent="$(get_user_agent)"
-    echo -e "\ncurl ${curl_options[@]} -H \"User-agent: ${user_agent}\" ${commoncrawl_url} | jq --raw-output .[0]'.\"cdx-api\"'" >> "${log_execution_file}"
+    echo -e "\ncurl ${curl_options[@]} -H \"User-agent: ${user_agent}\" \"${commoncrawl_url}\" | jq --raw-output .[0]'.\"cdx-api\"'" >> "${log_execution_file}"
     commoncrawl_db=$(curl "${curl_options[@]}" -H "User-agent: ${user_agent}" "${commoncrawl_url}" | jq --raw-output .[0]'."cdx-api"' 2>> "${log_execution_file}")
     echo "${commoncrawl_db}" >> "${log_execution_file}"
     echo "curl ${curl_options[@]} -H \"User-agent: ${user_agent}\" \"${commoncrawl_db}?url=*.${domain}/&output=json\"" >> "${log_execution_file}"
--- sources/hackerone.sh
+++ sources/hackerone.sh
@@ -13,7 +13,7 @@ hackerone-src(){
     user_agent="$(get_user_agent)"
     # Creates the JSON payload using the GraphQL query.
     payload='{"query":"query {team(handle:\"'"${domain}"'\"){structured_scopes{edges{node{asset_identifier}}}}}"}'
-    echo "curl ${curl_options[@]} -H \"User-agent: ${user_agent}\" -H \"Content-Type: application/json\" -d \"${payload}\" -X POST https://hackerone.com/graphql -o ${tmp_dir}/hackerone_output.json" >> "${log_execution_file}"
+    echo "\ncurl ${curl_options[@]} -H \"User-agent: ${user_agent}\" -H \"Content-Type: application/json\" -d '${payload}' -X POST \"https://hackerone.com/graphql\" -o ${tmp_dir}/hackerone_output.json" >> "${log_execution_file}"
     # Make the POST requ est using curl.
     curl "${curl_options[@]}" -H "User-agent: ${user_agent}" -H "Content-Type: application/json" -d "${payload}" -X POST "https://hackerone.com/graphql" -o "${tmp_dir}/hackerone_output.json" 2>> "${log_execution_file}"
     echo "Done!"
