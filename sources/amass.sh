24f9f628673b10079134f1a48b36948915c326b3
shadowdzn <255947222+shadowdzn@users.noreply.github.com>
2026-05-08 15:49:16 -0300

    update Amass to v5 syntax and fix variables
    - Atualização do  Amass para v5 em amass.sh e bruteforce.sh.
    - Remoção da flag obsoleta '-o' e implementação do 'amass subs -names' para extração de dados.

--- sources/amass.sh
+++ sources/amass.sh
@@ -10,10 +10,21 @@
 
 amass-src(){
     echo -ne "${yellow}$(date +"%d/%m/%Y %H:%M")${reset} ${red}>>${reset} Executing amass... "
-    echo -e "\namass enum ${amass_options[@]} -d ${domain} -o ${tmp_dir}/amass_active_output.txt" >> "${log_execution_file}"
-    echo "amass enum ${amass_options[@]} -passive -d ${domain} -o ${tmp_dir}/amass_passive_output.txt" >> "${log_execution_file}"
-    amass enum "${amass_options[@]}" -d "${domain}" -o "${tmp_dir}/amass_active_output.txt" 2>> "${log_execution_file}"
-    amass enum "${amass_options[@]}" -passive -d "${domain}" -o "${tmp_dir}/amass_passive_output.txt" 2>> "${log_execution_file}"
+    
+    echo -e "\namass enum ${amass_options[@]} -d ${domain}" >> "${log_execution_file}"
+    echo "amass enum ${amass_options[@]} -passive -d ${domain}" >> "${log_execution_file}"
+    
+    # ADICIONE ESTA LINHA PARA DEBUG
+    echo "DEBUG: Dominio é ${domain} e TMP é ${tmp_dir}" >> "${log_execution_file}"
+    
+    echo -ne "${yellow}$(date +"%d/%m/%Y %H:%M")${reset} ${red}>>${reset} Executing amass... "
+
+    amass enum "${amass_options[@]}" -d "${domain}" 2>> "${log_execution_file}"
+    amass enum "${amass_options[@]}" -passive -d "${domain}" 2>> "${log_execution_file}"
+    sleep 3
+    amass subs -names -d "${domain}" > "${tmp_dir}/amass_active_output.txt" 2>> "${log_execution_file}"
+    cp "${tmp_dir}/amass_active_output.txt" "${tmp_dir}/amass_passive_output.txt"
+    
     echo "Done!"
     sleep 1
 }
